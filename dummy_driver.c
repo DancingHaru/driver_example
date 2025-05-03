f1a5f3447c24d9edc69991fdfdf49d00d4ef3ee5
DancingHaur <bluepikachu@protonmail.com>
2025-05-03 15:48:35 +0200

    added 1sec task

--- dummy_driver.c
+++ dummy_driver.c
@@ -1,17 +1,50 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/workqueue.h>
+
+static int major;
+static struct workqueue_struct *wq;
+static struct delayed_work dw;
 
 MODULE_LICENSE("GPL");
+
+static struct file_operations fops= {};
+
+static void dummy_work_handler(struct work_struct *work)
+{
+	printk(KERN_INFO "dummy_print");
+	queue_delayed_work(wq, &dw, HZ);
+}
 
 static int dummy_init(void)
 {
 	printk(KERN_INFO "dummy-driver - Initialization\n");
+	//register device
+	major = register_chrdev(0, "dummy_driver", &fops);
+	if(major < 0)
+	{
+		printk(KERN_ERR "dummy_driver - registering character device failed.\n");
+		return major;
+	}
+	printk(KERN_INFO "dummy_driver - Registered with major number %d\n", major);
+
+	//initialize work to queue
+	wq = alloc_workqueue("wq", WQ_UNBOUND, 0);
+	INIT_DELAYED_WORK(&dw, dummy_work_handler);
+	queue_delayed_work(wq, &dw, HZ);
 	return 0;
 }
 
 static void dummy_exit(void)
 {
-	printk(KERN_INFO "dummy-driver - Uninitializing\n");
+	printk(KERN_INFO "dummy_driver - Uninitializing\n");
+	//destroy workqueue
+	if(wq)
+	{
+		destroy_workqueue(wq);
+	}
+	unregister_chrdev(major, "dummy_driver");
 }
 
 
